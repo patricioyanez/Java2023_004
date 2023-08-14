17d319a9a0c9d4256387d43da83660eff65165d3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-14 16:40:20 -0400

    clase 1

---
+++ PrimerEjemplo/src/Pizza.java
@@ -0,0 +1,33 @@
+// comentario
+
+/*
+    comentario más de una línea
+*/
+public class Pizza {
+    // atributos
+    private String nombre;
+    private String tamano;
+    private String masa;
+    private int precio;
+    
+    // constructores
+    public Pizza() // sin parametros
+    {
+        nombre = "";
+        tamano = "personal";
+        masa = "";
+        precio = 0;
+    }
+    public Pizza(String nombre, String tamano, String masa, int precio)
+    {
+        this.nombre = nombre;
+        this.tamano = tamano;
+        this.masa = masa;
+        this.precio = precio;
+    }
+    
+    
+    // getter and setter (accesadores y mutadores)
+    
+    // métodos customer (personalizado)
+}
