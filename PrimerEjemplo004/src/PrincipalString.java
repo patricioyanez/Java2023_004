ed7360fb8a89866b1027133592a0a1651ccc74c9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-08-29 17:35:02 -0400

    Clase 4

--- PrimerEjemplo004/src/PrincipalString.java
+++ PrimerEjemplo004/src/PrincipalString.java
@@ -15,11 +15,32 @@ public class PrincipalString {
             System.out.println("ES igual");
         else
             System.out.println("No es igual");
+        
         String texto = "        HOLA mundo       ";
         System.out.println("TRIM:" + texto);
         System.out.println("TRIM:" + texto.trim());
         
         System.out.println("Mayúscula : " + texto.toUpperCase());
         System.out.println("Minúscula : " + texto.toLowerCase());
+        
+        System.out.println("****** substring  *******");
+        System.out.println("Fernanda".substring(4));
+        System.out.println("Fernanda".substring(1,5));
+        
+        System.out.println("*** Index of ******");
+        System.out.println("abcd efg".indexOf(" "));
+        
+        System.out.println("*** replace ******");
+        System.out.println(texto.replace("HOLA", "Chao"));
+        
+        
+        //Ejercicios:
+        // Mostrar el primer nombre de las siguientes personas
+        // Juan Carlos
+        // Fernanda Maria
+        // Ana Maria
+        
+        String aux = "Fernanda Maria";
+        System.out.println(aux.substring(0, aux.indexOf(" ")));
     }
 }
