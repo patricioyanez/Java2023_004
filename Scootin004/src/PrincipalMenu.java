b485c8d79979b9785a1bfff06cf94dbd2711c47b
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-02 16:15:00 -0300

    Ejericicio menu

---
+++ Scootin004/src/PrincipalMenu.java
@@ -0,0 +1,12 @@
+
+public class PrincipalMenu {
+                    //Ejercicio 3
+        // Crear un menu con la opciones:
+        // - Ingresar empleado, buscar si existe, 
+        // - Listar todo, 
+        // - Listar por puesto (seleccionar que puesto), 
+        // - Eliminar por rut y salir.
+    public static void main(String[] args) {
+        
+    }
+}
