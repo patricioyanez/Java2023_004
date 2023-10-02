33e87cfe2ad96956d46d33b1f7fd51b83702a724
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-02 15:31:09 -0300

    update

--- Scootin004/src/Principal.java
+++ Scootin004/src/Principal.java
@@ -38,5 +38,12 @@ public class Principal {
             System.out.println(aux.imprimir());
         else
             System.out.println("Empleado no encontrado");
+        
+                //Ejercicio 3
+        // Crear un menu con la opciones:
+        // - Ingresar empleado, buscar si existe, 
+        // - Listar todo, 
+        // - Listar por puesto (seleccionar que puesto), 
+        // - Eliminar por rut y salir.
     }
 }
