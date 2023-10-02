80bf4b506198730c51401c1f7ed5e01ebfb250ab
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-02 16:17:50 -0300

    update

--- Scootin004/src/PrincipalMenu.java
+++ Scootin004/src/PrincipalMenu.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class PrincipalMenu {
                     //Ejercicio 3
@@ -7,6 +8,25 @@ public class PrincipalMenu {
         // - Listar por puesto (seleccionar que puesto), 
         // - Eliminar por rut y salir.
     public static void main(String[] args) {
-        
+        Scanner leer = new Scanner(System.in);
+        Empresa empresa = new Empresa();
+        String opcion = "";
+        while(!opcion.equals("5"))
+        {
+            System.out.println("* * * M E N Ú * * *");
+            System.out.println("1.- Ingresar empleado");
+            System.out.println("2.- Listar empleados");
+            System.out.println("3.- Listar Ejecutivos");
+            System.out.println("4.- Eliminar por rut");
+            System.out.println("5.- Salir");
+            opcion = leer.next();
+            
+            if(opcion.indexOf("12345")<0)
+                System.out.println("Opción no válida");
+            else if(opcion.equals("1"))
+            {
+                
+            }
+        }
     }
 }
